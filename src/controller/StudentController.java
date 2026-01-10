3735d554d1d02a49685b1b50a3a59a4c963d53b0
Kiran Dahal <dahal.utsav63@gmail.com>
2026-01-10 21:28:05 +0545

    complete StudentController

--- src/controller/StudentController.java
+++ src/controller/StudentController.java
@@ -4,6 +4,7 @@
  */
 package controller;
 import java.util.ArrayList;
+import java.util.HashMap;
 import model.Student;
 /**
  *
@@ -11,43 +12,38 @@ import model.Student;
  */
 public class StudentController 
 {
-    private static ArrayList<Student> studentList = new ArrayList<>();
+    private static HashMap<String, Student> studentMap = new HashMap<>();
 
-    public String registerNewStudent(String studentId, String email, String password) 
-    {
-        for (Student s : studentList) 
-        {
-            if (s.getStudentId().equalsIgnoreCase(studentId)) 
-            {
-                return "Student with this ID is already registered";
-                
-            }
-            else if (s.getEmail().equalsIgnoreCase(email)) 
-            {
-                return "Student with this email is already registered";
-            }
-            else if (s.getPassword().equals(password) || password.length()<6) 
-            {
-                return "Please use Strong password";
-            }            
+    public String registerNewStudent(String studentId, String email, String password) {
+        
+        if (studentMap.containsKey(studentId)) {
+            return "Student with this College ID already exists!!";
         }
+
+        if (password.length() < 5) {
+            return "Not a Strong Password! \nPassword must be more than 5 characters long!!";
+        }
+        for (Student s : studentMap.values()) {
+            if (s.getEmail().equalsIgnoreCase(email)) {
+                return "User with this email already exists!!";
+            }
+            if (s.getPassword().equals(password)) {
+                return "Please enter a unique password!!";
+            }
+        }
+
+        // Create new student and add to map
         Student newStudent = new Student(studentId, email, password);
-        studentList.add(newStudent);
-        return null;
-    }
-    
-    public static boolean loginStudent(String email, String password) {
+        studentMap.put(studentId, newStudent);
 
-        for (Student s : studentList) {
-            if (s.getStudentId().equalsIgnoreCase(email) && s.getPassword().equals(password)) {
-                return true; 
-            }
+        return null; // null means registration successful
+    }
+
+    public static boolean loginStudent(String studentId, String password) {
+        if (!studentMap.containsKey(studentId)) {
+            return false;
         }
-        return false; 
-    }
-
-    public static ArrayList<Student> getStudentList() {
-        return studentList;
-    }
-    
+        Student s = studentMap.get(studentId);
+        return s.getPassword().equals(password);
+    }    
 }
